f74e02f43d78471a0cc13a5fb1cb64aad77a9568
9noeyni9 <koo949494@naver.com>
2025-05-02 15:02:39 +0900

    :sparkles: feat: 네이버 설정 추가
    - 테스트 완료

--- roome/src/main/java/com/roome/global/oauth/user/factory/OAuth2UserInfoFactory.java
+++ roome/src/main/java/com/roome/global/oauth/user/factory/OAuth2UserInfoFactory.java
@@ -2,6 +2,7 @@ package com.roome.global.oauth.user.factory;
 
 import com.roome.global.oauth.user.userinfo.GoogleOAuth2UserInfo;
 import com.roome.global.oauth.user.userinfo.KakaoOAuth2UserInfo;
+import com.roome.global.oauth.user.userinfo.NaverOAuth2UserInfo;
 import com.roome.global.oauth.user.userinfo.OAuth2UserInfo;
 
 import java.util.Map;
@@ -11,7 +12,7 @@ public class OAuth2UserInfoFactory {
         return switch (provider) {
             case "google" -> new GoogleOAuth2UserInfo(attributes);
             case "kakao" -> new KakaoOAuth2UserInfo(attributes);
-//            case "naver" -> new NaverOAuth2UserInfo(attributes);
+            case "naver" -> new NaverOAuth2UserInfo(attributes);
             default -> throw new IllegalArgumentException("지원하지 않는 소셜 로그인입니다.");
         };
     }
---
+++ roome/src/main/java/com/roome/global/oauth/user/userinfo/NaverOAuth2UserInfo.java
@@ -0,0 +1,41 @@
+package com.roome.global.oauth.user.userinfo;
+
+import com.roome.domain.user.entity.Provider;
+import lombok.RequiredArgsConstructor;
+
+import java.util.Map;
+
+@RequiredArgsConstructor
+public class NaverOAuth2UserInfo implements OAuth2UserInfo{
+
+    private final Map<String, Object> attributes;
+    @Override
+    public Provider getProvider() {
+        return Provider.NAVER;
+    }
+
+    @Override
+    public String getProviderId() {
+        return (String) ((Map) attributes.get("response")).get("id");
+    }
+
+    @Override
+    public String getEmail() {
+        return (String) ((Map) attributes.get("response")).get("email");
+    }
+
+    @Override
+    public String getName() {
+        return (String) ((Map) attributes.get("response")).get("nickname");
+    }
+
+    @Override
+    public String getNickname() {
+        return (String) ((Map) attributes.get("response")).get("nickname");
+    }
+
+    @Override
+    public String getProfileImage() {
+        return (String) ((Map) attributes.get("response")).get("profile_image");
+    }
+}
