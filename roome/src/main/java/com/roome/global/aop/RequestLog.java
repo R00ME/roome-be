64c5e1f7c38b2501069bd92dfbab7d5570937398
9noeyni9 <koo949494@naver.com>
2025-07-13 17:32:58 +0900

    :sparkles: feat: RequestLog 개발중

---
+++ roome/src/main/java/com/roome/global/aop/RequestLog.java
@@ -0,0 +1,29 @@
+package com.roome.global.aop;
+
+import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+import org.springframework.web.context.request.RequestContextHolder;
+import org.springframework.web.context.request.ServletRequestAttributes;
+
+@Slf4j
+@Aspect
+@Component
+public class RequestLog {
+
+    @Pointcut("execution(* com.roome.domain.*.controller..*.*(..))")
+    private void cut() {
+
+    }
+
+    @Before("cut()")
+    public void logBefore(JoinPoint joinPoint) {
+        ServletRequestAttributes attributes = (ServletRequestAttributes) RequestContextHolder.getRequestAttributes();
+        HttpServletRequest request = attributes.getRequest();
+
+    }
+}
