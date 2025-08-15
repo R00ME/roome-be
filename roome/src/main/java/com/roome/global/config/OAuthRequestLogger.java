0af82e9eae8c3ca56d6c0e3e4377253f2ca02511
9noeyni9 <koo949494@naver.com>
2025-08-15 18:52:48 +0900

    :recycle: fix: log 출력 형식 변경

--- roome/src/main/java/com/roome/global/config/OAuthRequestLogger.java
+++ roome/src/main/java/com/roome/global/config/OAuthRequestLogger.java
@@ -2,17 +2,19 @@ package com.roome.global.config;
 
 import jakarta.servlet.*;
 import jakarta.servlet.http.HttpServletRequest;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 
+@Slf4j
 @Component
 public class OAuthRequestLogger implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
         if (req.getRequestURI().contains("/oauth2/authorization")) {
-            System.out.println("[Incoming OAuth request]: " + req.getRequestURL());
+            log.info("[Incoming OAuth request]: {}", req.getRequestURL());
         }
         chain.doFilter(request, response);
     }
